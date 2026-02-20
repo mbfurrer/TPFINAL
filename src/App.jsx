b77bfd8fac90e2d84bb409015a683c15a7b7ee6c
mbfurrer <m.furrerbeltran@gmail.com>
2026-02-20 11:42:22 -0300

    plantillaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router'
 import HomeScreen from './Screens/HomeScreen/HomeScreen'
 import ContactScreen from './Screens/ContactScreen/ContactScreen'
--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ContactSideBar from '../../Components/ContactSideBar/ContactSideBar'
 
 export default function HomeScreen() {
-  return (
-    <div>
-      <ContactSideBar/>
-      </div>
-  )
+    return (
+        <div>
+            <ContactSideBar />
+        </div>
+    )
 }
