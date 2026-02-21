004d0511fe6580b4b6c30fe1e78bd079e37b9f48
mica.furrer <m.furrerbeltran@gmail.com>
2026-02-21 20:21:57 -0300

    estilosshome

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -35,23 +35,31 @@ export default function HomeScreen() {
 
             </div>
             <div className='contact-sidebar'>
-            <ContactSideBar />
+                <ContactSideBar />
             </div>
             <div className='message-sidebar'>
                 <div className='download-card'>
+                    <i class="bi bi-cloud-download"></i>
                     <h1>Download WhatsApp for Windows</h1>
                     <span>Make calls, share your screen and get a faster experience when you download the Windows app.</span>
-                    <button>Download</button>
+                    <a href="https://www.whatsapp.com/download"
+                        target="_blank"
+                        class="download-btn">
+                        Download
+                    </a>
                 </div>
-                    <button>
+
+                <div className='quick-actions'>
+                    <button className='quick-actions-btn'>
                         <i class="bi bi-file-earmark-text"></i>
                         <span>Send document</span>
                     </button>
-                    <button>
+                    <button className='quick-actions-btn'>
                         <i class="bi bi-person-plus"></i>
-                        <span></span>
+                        <span>Add contact</span>
                     </button>
                 </div>
+            </div>
         </div>
     )
 }
