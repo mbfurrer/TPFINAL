9d1cb55ce9ae88c0d96888e84da198a54bf2d409
mbfurrer <m.furrerbeltran@gmail.com>
2026-02-27 23:29:36 -0300

    readyoploadv

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -2,7 +2,7 @@
 const contacts = [
     {
         id: 1,
-        phone: "1162783212",
+        phone: "1123456789",
         type: "ind",
         name: "Mama",
         favorite: true,
@@ -13,28 +13,28 @@ const contacts = [
                 id: 1,
                 text: 'Hola Mica, le diste de comer al perro ya?',
                 send_by_me: false, 
-                created_at: '2024-06-01T12:00:00Z',
+                created_at: '09:31',
                 is_read: true
             },
             {
                 id: 2,
                 text: 'Si, ya comio ma',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '09:35',
                 is_read: true
             },
             {
                 id: 3,
                 text: 'Gracias, por favor compra bananas cuando vuelvas',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '09:36',
                 is_read: false
             }
         ]
     },
     {
         id: 2,
-        phone: "1163783212",
+        phone: "1155667788",
         type: "ind",
         name: 'Juan UTN',
         favorite: false,
@@ -45,28 +45,28 @@ const contacts = [
                 id: 1,
                 text: 'Holaaa, como venis con el tp?',
                 send_by_me: true, 
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '22:35',
                 is_read: true
             },
             {
                 id: 2,
                 text: 'Hola! Estoy en el horno jajaja',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '23:02',
                 is_read: true
             },
             {
                 id: 3,
                 text: 'XD same',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '23:05',
                 is_read: true
             }
         ]
     }, 
     {
         id: 3,
-        phone: "11333212",
+        phone: "1133990088",
         type: "ind",
         name: 'Cata',
         favorite: true,
@@ -77,42 +77,42 @@ const contacts = [
                 id: 1,
                 text: 'Hola buenas',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z', 
+                created_at: '14:34', 
                 is_read: true
             },
             {
                 id: 2,
                 text: 'Hola, como va?',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '15:04',
                 is_read: true
             }, 
             {
                 id: 3,
                 text: 'bien bien, vas a ir a la fiesta de Juan?',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '15:05',
                 is_read: true
             },
             {
                 id: 4,
                 text: 'Sii, vamos juntas? ',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '15:05',
                 is_read: true
             },
             {
                 id: 5,
                 text: 'dale!',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '15:06',
                 is_read: true
             }
         ]
     },
     {
         id: 4,
-        phone: "11833212",
+        phone: "1123456234",
         type: "ind",
         name: 'Santi',
         favorite: true,
@@ -123,35 +123,35 @@ const contacts = [
                 id: 1,
                 text: 'Mica, podrás pasarme el numero de May? Se me borró',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z', 
+                created_at: '19:00', 
                 is_read: true
             },
             {
                 id: 2,
                 text: 'Sii obvio, ya lo busco',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '19:06',
                 is_read: true
             }, 
             {
                 id: 3,
                 text: 'Es este: 1145632712',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '19:06',
                 is_read: true
             },
             {
                 id: 4,
                 text: 'Gracias! Sabes si hoy anda por CABA? Para visitarla ',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '19:07',
                 is_read: true
             },
             {
                 id: 5,
                 text: 'Creo que si!',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '19:07',
                 is_read: true
             }
         ]
@@ -169,35 +169,35 @@ const contacts = [
                 id: 1,
                 text: 'Mica, estas en casa ya?',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z', 
+                created_at: '17:35', 
                 is_read: true
             },
             {
                 id: 2,
                 text: 'No no, llego en 1 hora mas o menos',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '17:35',
                 is_read: true
             }, 
             {
                 id: 3,
                 text: 'Ah bueno, puedo usar tu compu mientras?',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '17:40',
                 is_read: true
             },
             {
                 id: 4,
                 text: 'Si dale np',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '17:41',
                 is_read: true
             }
         ]
     },
     {
         id: 6,
-        phone: "11830712",
+        phone: "1145632712",
         type: "ind",
         name: 'May',
         favorite: true,
@@ -208,35 +208,35 @@ const contacts = [
                 id: 1,
                 text: 'Mica, te puedo mandar el texto del post para que lo revises?',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z', 
+                created_at: '20:04', 
                 is_read: true
             },
             {
                 id: 2,
                 text: 'Dale',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '20:05',
                 is_read: true
             }, 
             {
                 id: 3,
                 text: 'Mandalo y lo veo',
                 send_by_me: true,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '20:05',
                 is_read: true
             },
             {
                 id: 4,
                 text: 'El gobierno anunció nuevas medidas económicas destinadas a estimular el crecimiento y reducir la inflación. Especialistas señalan que el impacto dependerá de la implementación y la respuesta del mercado. Mientras tanto, sectores productivos evalúan oportunidades y riesgos ante un escenario internacional todavía incierto.',
                 send_by_me: false,
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '20:15',
                 is_read: false
             }
         ]
     },
     {
         id: 7,
-        phone: "11830712 , 11833212, 1147467585",
+        phone: "May , Sergio, Santi",
         type: "group",
         name: "Los Bros",
         favorite: true,
@@ -249,7 +249,7 @@ const contacts = [
                 text: 'Holaaa nos vemos hoy?',
                 send_by_me: true,
                 send_by: "Me", 
-                created_at: '2024-06-01T12:00:00Z',
+                created_at: '13:07',
                 is_read: true
             },
             {
@@ -257,7 +257,7 @@ const contacts = [
                 text: 'Dalee a que hora y donde?',
                 send_by_me: false,
                 send_by: "May",
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '13:20',
                 is_read: true
             },
             {
@@ -265,7 +265,7 @@ const contacts = [
                 text: 'Yo estoy',
                 send_by_me: false,
                 send_by: "Santi",
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '13:20',
                 is_read: true
             },
             {
@@ -273,14 +273,14 @@ const contacts = [
                 text: 'Tipo 4 vamos a la plaza cerca de lo de May',
                 send_by_me: true,
                 send_by: "Me", 
-                created_at: '2024-06-01T12:00:00Z',
+                created_at: '13:21',
                 is_read: true
             },
         ]
     },
     {
         id: 8,
-        phone: "1183673412 , 1162783212",
+        phone: "Mama , Flor",
         type: "group",
         name: "Familia",
         favorite: false,
@@ -293,7 +293,7 @@ const contacts = [
                 text: 'Acuerdense de que hay que comprar un regalo para papa',
                 send_by_me: false,
                 send_by: "Mama", 
-                created_at: '2024-06-01T12:00:00Z',
+                created_at: '12:45',
                 is_read: true
             },
             {
@@ -301,7 +301,7 @@ const contacts = [
                 text: 'Ok, le compramos un perfume como siempre?',
                 send_by_me: false,
                 send_by: "Flor",
-                created_at: '2024-06-01T12:05:00Z',
+                created_at: '12:55',
                 is_read: false
             },
         ]
